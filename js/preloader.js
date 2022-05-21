97ea7ec0c735ee53fadb0b22451f0a9825b93369
Ashfaque Hossain Abir <104485722+Abirett@users.noreply.github.com>
2022-05-21 21:58:35 +0600

    Add files via upload

---
+++ js/preloader.js
@@ -0,0 +1,15 @@
+$(window).on("load", function () {
+    "use strict";
+  
+    /*=========================================================================
+          Preloader
+      =========================================================================*/
+    $("#preloader").delay(1550).fadeOut("slow");
+    // Because only Chrome supports offset-path, feGaussianBlur for now
+    var isChrome =
+      /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  
+    if (!isChrome) {
+      document.getElementsByClassName("infinityChrome")[0].style.display = "none";
+      document.getElementsByClassName("infinity")[0].style.display = "block";
+    }})
